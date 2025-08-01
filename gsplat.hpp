fb724c9faba61e7e0c18bbc9ab3cf21eef5dc16b
Eurasia32 <54104187+Eurasia32@users.noreply.github.com>
2025-08-01 14:29:34 +0800

    Update

--- gsplat.hpp
+++ gsplat.hpp
@@ -1,12 +1,12 @@
 #ifndef GSPLAT_H
 #define GSPLAT_H
 
-#include <rasterizer/gsplat/config.h>
+#include "./rasterizer/gsplat/config.h"
 
 #if defined(USE_HIP) || defined(USE_CUDA)
-#include <rasterizer/gsplat/bindings.h>
+#include "./rasterizer/gsplat/bindings.h"
 #endif
 
-#include <rasterizer/gsplat-cpu/bindings.h>
+#include "./rasterizer/gsplat-cpu/bindings.h"
 
 #endif
